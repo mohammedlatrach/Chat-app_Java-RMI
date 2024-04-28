0268d0defee5814da2e1802457a08a60dfecbd4f
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-04-28 20:33:07 +0100

    before structuring the project

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 import server.Chat;
 
@@ -17,7 +18,32 @@ public class ChatClient {
 		// TODO Auto-generated method stub
 		
 		Chat chatService =(Chat)Naming.lookup("rmi://localhost:1099/remoteChatObject");
-		System.out.println(chatService.test());
+		
+		
+		
+		Scanner chatPrompt = new Scanner(System.in);
+		boolean myApp = true;
+		
+		while(myApp) {
+			System.out.println("Bienvenue dans le menu de l'application");
+
+			System.out.println("Pour se connecter envoyer 1");
+			System.out.println("Pour sortir cliquer envoyer 0");
+			int userAnswer = chatPrompt.nextInt();
+			
+			if(userAnswer==0) {
+				myApp=false;
+				break;
+			}
+			
+			if(userAnswer==1) {
+				System.out.println("Bienvenue dans l'application de Chat");
+				break;
+			}
+		}
+		
+		
+		//System.out.println(chatService.test());
 	}
 
 }
